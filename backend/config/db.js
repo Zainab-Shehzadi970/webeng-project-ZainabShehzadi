8601dfaf83189139de7f652f4fcf07d0ffef7bf3
Zainab-Shehzadi970 <zainabshehzadi970@gmail.com>
2026-05-25 16:56:52 +0500

    Correction

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
-const env = require('./env'); // ✅ import
 
 const db = mysql.createConnection({
-  host: env.DB_HOST,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
